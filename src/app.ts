cfa36e31d8088c4e9a54e64d1674922bfdc16c24
Tim Turner <timdturner@gmail.com>
2017-07-28 17:35:25 -0400

    Update title, add webpack watch script

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import { ConfiguresRouter, Router, RouterConfiguration} from 'aurelia-router';
 
 export class App implements ConfiguresRouter {
     configureRouter(config: RouterConfiguration, router: Router) {
-        config.title = "Aurelia Test App";
+        config.title = "Today";
 
         config.map([
-            {route: '', moduleId: PLATFORM.moduleName('./pages/home'), name: 'home', title: 'Home' }
+            { route: '', moduleId: PLATFORM.moduleName('./pages/home'), name: 'home' }
         ]).mapUnknownRoutes(PLATFORM.moduleName('./pages/not-found'));
     }
 }
