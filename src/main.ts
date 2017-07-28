dcb1bc3aa3b56caed64ab436cf905de4c65cd5c4
Tim Turner <timdturner@gmail.com>
2017-07-28 19:15:05 -0400

    Add aurelia-oauth and configure

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,39 @@
 import { Aurelia, PLATFORM } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-fetch-client';
+import { OAuthService, OAuthTokenService } from 'aurelia-oauth';
 
 export function configure(aurelia: Aurelia): void {
-    aurelia.use.standardConfiguration();
+    const httpClient = <HttpClient>aurelia.container.get(HttpClient);
+    
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging()
+        .plugin(PLATFORM.moduleName('aurelia-oauth'), (oauthService: OAuthService, oauthTokenService: OAuthTokenService, configureClient: any) =>
+            configureOAuth(oauthService, oauthTokenService, configureClient, httpClient));
+
 
     aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+}
+
+function configureOAuth(oauthService: OAuthService, oauthTokenService: OAuthTokenService, configureClient: (client: any) => void, client: any) {
+    oauthService.configure(
+        {
+            loginUrl: 'https://todoist.com/oauth/authorize',
+            logoutUrl: '',
+            clientId: 'bdf2c7fb5a1e4467af686143ea008331', //TODO: Put this in a config file
+            scope: 'data:read_write',
+            state: 'secretstring' //TODO: Put this in a config file
+        }
+    );
+
+    oauthTokenService.configure(
+        {
+            name: 'token id_token',
+            urlTokenParameters: {
+                idToken: 'id_token'
+            }
+        }
+    );
+
+    configureClient(client);
 }
--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,3 +1,11 @@
+import { autoinject } from 'aurelia-framework';
+import { OAuthService } from 'aurelia-oauth';
+
+@autoinject
 export class Home {
+
+    constructor(oauthService: OAuthService) {
+        oauthService.login();
+    }
     message: string;
 }
